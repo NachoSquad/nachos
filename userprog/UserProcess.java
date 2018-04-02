57cf6109be87086f16c309e59cddcd119aff7330
TheEncarnate <anasser2@ucmerced.edu>
2018-04-02 13:42:08 -0700

    fds array intialized

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -29,8 +29,14 @@ public class UserProcess {
     public UserProcess() {
 	int numPhysPages = Machine.processor().getNumPhysPages();
 	pageTable = new TranslationEntry[numPhysPages];
-	for (int i=0; i<numPhysPages; i++)
+	for (int i=0; i<numPhysPages; i++) {
 	    pageTable[i] = new TranslationEntry(i,i, true,false,false,false);
+	}
+	
+	for(int j = 0; j < fds.length; j++) {
+		fds[j] = new FileDescriptor(); 
+	}
+	
     }
     
     /**
