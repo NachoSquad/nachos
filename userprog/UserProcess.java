a52f939cdf72f6b6e0bfa9347850989bab1a1770
Shubham Naik <shubham.naik10@gmail.com>
2018-04-02 23:45:45 -0700

    add breakpoints

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -607,20 +607,21 @@ private int handleJoin(int childProcessId, int status) {
 
 		// join thread
 		childProcess.thread.join();
-
+		Lib.debug(dbgProcess, Type + "Break 1");
 		// unregister the child
 		UserKernel.unregisterProcess(childProcessId);
-
+		Lib.debug(dbgProcess, Type + "Break 2");
 		// store exit status
 		byte temp[] = new byte[4];
 		temp = Lib.bytesFromInt(childProcess.exitStatus);
 		int cntBytes = writeVirtualMemory(status, temp);
-
+		Lib.debug(dbgProcess, Type + "Break 3");
 		if (cntBytes != 4) {
 			return 1;
-		} else {
-			return 0;
 		}
+
+		Lib.debug(dbgProcess, Type + "Break 4");
+		return 0;
 	}
 	private int handleExit(int exitStatus) {
 		Lib.debug(dbgProcess, "handleExit()");
